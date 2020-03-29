a77e33da818b255c867edc1838b0f95930346d25
Asad Siddiqui <msasad8@gmail.com>
2020-03-29 22:12:26 +0530

    Change conflicting name self.log -> self.l

--- logger.py
+++ logger.py
@@ -1,28 +1,24 @@
-from mitmproxy import ctx
 from models import Log
 import json
 
 
 class Logger:
 
+
     def response(self, flow):
-        self.log.response_body = flow.response.get_text()
-        self.log.response_headers = json.dumps(dict(flow.response.headers))
-        self.log.rtt = flow.request.timestamp_end - flow.request.timestamp_start
-        self.log.code = flow.response.status_code
-        ctx.log.info(str(flow.request.method))
-        ctx.log.info(str(flow.request.url))
-        self.log.save()
+        self.l.response_body = flow.response.get_text()
+        self.l.response_headers = json.dumps(dict(flow.response.headers))
+        self.l.rtt = flow.request.timestamp_end - flow.request.timestamp_start
+        self.l.code = flow.response.status_code
+        self.l.save()
 
     def request(self, flow):
-        self.log = Log()
-        self.log.request_body = flow.request.get_text()
-        self.log.method = flow.request.method
-        self.log.url = flow.request.url
-        ctx.log.info(flow.request.headers.get('X-Application-Id'))
-        del flow.request.headers['X-Application-Id']
-        self.log.request_headers = json.dumps(dict(flow.request.headers))
-        self.log.save()
+        self.l = Log()
+        self.l.request_body = flow.request.get_text()
+        self.l.method = flow.request.method
+        self.l.url = flow.request.url
+        self.l.request_headers = json.dumps(dict(flow.request.headers))
+        self.l.save()
 
 
 addons = [
